871f4c6446b37798769d091fbe66cbd124755d05
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-12-09 17:45:12 +0300

    Initial Commit

---
+++ Adding to the Beast List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adding to the Beast List
+//
+//  Created by Atheer Alahmari on 05/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
